01a90f9f92e0ecd156ae25f49c35a896440496c3
Inam05 <inamulhassan.tech@gmail.com>
2025-08-16 09:15:51 +0500

    Path and Query Parameters added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -20,3 +20,15 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id : str = Path(..., description='ID of the patient in the DB',example='P001')):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail='Patient not found!')
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight and bmi'), order: str = Query('acc', description= 'Sort in accending or descending order.')):
+
+    valid_fields = 
